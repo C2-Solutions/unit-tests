4df4b562f8b5a84212f3f49c6b057a0a343cc594
LightArrowsEXE <Lightarrowsreboot@gmail.com>
2022-05-16 15:23:21 +0200

    Upload Palindrome assignments

---
+++ Palindrome/PalindromeChecker/PalindromeChecker.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Palindromes
+{
+    public class PalindromeChecker
+    {
+        public static bool IsPalindrome(string InputString)
+        {
+            string StringToCheck = Regex.Replace(InputString.ToLower(), @"[^a-z]+", "");
+            return StringToCheck.SequenceEqual(StringToCheck.Reverse());
+        }
+    }
+}
---
+++ Palindrome/TestPalindromeChecker/UnitTest1.cs
@@ -0,0 +1,89 @@
+﻿using Palindromes;
+using Xunit;
+
+namespace TestPalindromes
+{
+    public class UnitTest1
+    {
+        [Theory]
+        [InlineData("girafarig")]
+        [InlineData("dvd")]
+        [InlineData("lol")]
+        [InlineData("mom")]
+        [InlineData("anna-susanna")]
+        public void IsPalindrome(string InputString)
+        {
+            // Act
+            var result = PalindromeChecker.IsPalindrome(InputString);
+
+            // Assert
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void NotIsAPalindrome()
+        {
+            // Arrange
+            var input = "random";
+
+            // Act
+            var result = PalindromeChecker.IsPalindrome(input);
+
+            // Assert
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void IsPalindromeHasCapital()
+        {
+            // Arrange
+            var input = "Taco Cat";
+
+            // Act
+            var result = PalindromeChecker.IsPalindrome(input);
+
+            // Assert
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void IsPalindromeHasSpecial()
+        {
+            // Arrange
+            var input = "A0ibo*h()ph#!ob$^ia";
+
+            // Act
+            var result = PalindromeChecker.IsPalindrome(input);
+
+            // Assert
+            Assert.True(result);
+        }
+
+        [Theory]
+        [InlineData("Yo, banana boy!")]
+        [InlineData("A man, a plan, a canal – Panama")]
+        [InlineData("Cigar? Toss it in a can. It is so tragic.")]
+        [InlineData("Dog, as a devil deified, lived as a god.")]
+        public void IsPalindromeSentence(string InputString)
+        {
+            // Act
+            var result = PalindromeChecker.IsPalindrome(InputString);
+
+            // Assert
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void NotIsPalindromeSentence()
+        {
+            // Arrange
+            var input = "People die when they are killed.";
+
+            // Act
+            var result = PalindromeChecker.IsPalindrome(input);
+
+            // Assert
+            Assert.False(result);
+        }
+    }
+}
